289c6c65f0ccca2647e97f8588a6e715cbd6cc35
Ron <ronitzhakk1993@gmail.com>
2019-06-06 20:19:32 +0300

    CNN cleaned up

--- task1/src/CNNClassifier.py
+++ task1/src/CNNClassifier.py
@@ -1,95 +1,138 @@
-from keras.layers import Input, Conv2D, Conv1D, Activation, Add, Dense, \
-    Flatten, GlobalMaxPooling2D, GlobalMaxPooling1D, Concatenate, Embedding
-from keras.models import Model, Sequential
-from keras.backend import batch_flatten
-from keras.optimizers import Adam
-import gensim
-import nltk
+from keras.layers import Input, Conv1D, Dense, GlobalMaxPooling1D, \
+    Concatenate, Embedding
+from keras.models import Model, load_model
 from task1.src.ClassifierBase import ClassifierBase
 from task1.src.data_parser import Parser
-from task1.src.Commons import HANDLES_DICT
 import numpy as np
 import tensorflow as tf
 import os
 from task1.src.Commons import OUT_DIR_PATH, split_training_validation_sets
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from keras.utils import plot_model
+import pickle
 
+
+# I will show you da wegh
+MODEL_FILE_NAME = "model 0.75 acc"
+TOKENIZER_FILE_NAME = "tokenizer.pickle"
 
 class CNNClassifier(ClassifierBase):
+    """
+    Classifies using a convolutional network
+    """
     corpus = None
-    models = []
+    model = None
     tokenizer = None
+    model_built = False
+    model_trained = False
 
     def __init__(self) -> None:
+        """
+        Inits the object, loads model if already exists
+        """
         super().__init__()
+        if os.path.isfile(MODEL_FILE_NAME) and os.path.isfile(TOKENIZER_FILE_NAME):
+            self.model = load_model(MODEL_FILE_NAME)
+            with open('tokenizer.pickle', 'rb') as handle:
+                self.tokenizer = pickle.load(handle)
+            self.model_trained = True
 
-    @staticmethod
-    def zero_one_loss(y, y_hat):
-        return np.count_nonzero(y - y_hat)
 
     @staticmethod
     def canonize_y(y):
+        """
+        Canonizes y vec to proper form
+        :param y: Label vector
+        :return: Proper form of y for the CNN
+        """
         ret = np.zeros((y.shape[0], 10))
         ret[np.arange(y.shape[0]), y.astype(int)] = 1
         return ret
 
-    def fit(self, X, y):
-        self.corpus = Parser.tokenize_tweets(X)
-        tokenizer = Tokenizer()
-        self.tokenizer = tokenizer
-        tokenizer.fit_on_texts(X)
-        length = max([len(t.split()) for t in X])
-        size_of_vocabulary = len(tokenizer.word_index) + 1
-        X_tokens = tokenizer.texts_to_sequences(X)
-        X_train_pad = pad_sequences(X_tokens, maxlen=length, padding='post')
+    def build_model(self):
+        """
+        Build the model
+        """
+        size_of_vocabulary = len(self.tokenizer.word_index) + 1
         a = Input((None,))
-        b = Embedding(size_of_vocabulary, 120, input_length=length)(a)
-        #         gram_1 = Conv1D(7, (1,), padding="same")(b)
-        #         gram_1 = Conv1D(10, (1,), padding="same")(gram_1)
-        gram_2 = Conv1D(7, (2,), padding="same")(b)
-        gram_2 = Conv1D(10, (2,), padding="same")(gram_2)
-        gram_3 = Conv1D(7, (3,), padding="same")(b)
-        gram_3 = Conv1D(10, (3,), padding="same")(gram_3)
-        all = Concatenate()([gram_3, gram_2])
-        #         all = Activation('relu')(all)
+        b = Embedding(size_of_vocabulary, 120)(a)
+
+        gram_1 = Conv1D(8, (1,), padding="same")(b)
+        gram_1 = Conv1D(15, (5,), padding="same")(gram_1)
+
+        gram_2 = Conv1D(8, (2,), padding="same")(b)
+        gram_2 = Conv1D(15, (10,), padding="same")(gram_2)
+
+        gram_3 = Conv1D(8, (3,), padding="same")(b)
+        gram_3 = Conv1D(15, (15,), padding="same")(gram_3)
+
+        flatten_first = GlobalMaxPooling1D()(b)  # psuedo flatten
+        dense_first = Dense(400, activation=tf.nn.sigmoid)(flatten_first)
+        all = Concatenate()([gram_3, gram_2, gram_1])
         flatten = GlobalMaxPooling1D()(all)  # psuedo flatten
-        dense = Dense(300, activation=tf.nn.sigmoid)(flatten)
-        #         dense = Dense(300, activation=tf.nn.sigmoid)(dense)
-        out = Dense(10, activation=tf.nn.sigmoid)(dense)
+        all = Concatenate()([flatten, dense_first])
+        out = Dense(10, activation=tf.nn.sigmoid)(all)
 
         model = Model(inputs=a, outputs=out)
         model.summary()
         model.compile(optimizer='adam', loss='categorical_crossentropy',
                       metrics=['accuracy'])
-        model.fit(X_train_pad, CNNClassifier.canonize_y(y), epochs=6,
-                  steps_per_epoch=21)
-        self.models.append(model)
+        self.model = model
+        plot_model(model, to_file='model.png')
+        self.model_built = True
+
+    def fit(self, X, y):
+        """
+        Fit the model
+        :param X: Data
+        :param y: Labels
+        """
+        self.corpus = Parser.tokenize_tweets(X)
+        tokenizer = Tokenizer()
+        self.tokenizer = tokenizer
+        tokenizer.fit_on_texts(X)
+        length = max([len(t.split()) for t in X])
+        X_tokens = tokenizer.texts_to_sequences(self.corpus[0])
+        X_train_pad = pad_sequences(X_tokens, maxlen=length, padding='post')
+        self.build_model()
+        self.model.fit(X_train_pad, CNNClassifier.canonize_y(y), epochs=8,
+                       steps_per_epoch=15)
+        self.model_trained = True
+        with open(TOKENIZER_FILE_NAME, 'wb') as handle:
+            pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
     def classify(self, X):
-        pass
+        """
+        Classifies the given data using the CNN that has already been fitted
+        :param X: The data
+        :return: The prediction
+        """
+        Y = Parser.tokenize_tweets(X)
+        X = Y[0]
+        X_tokens = self.tokenizer.texts_to_sequences(X)
+        length = max([len(t.split()) for t in X])
+        X_train_pad = pad_sequences(X_tokens, maxlen=length, padding='post')
+
+        return self.model.predict(X_train_pad)
 
     def score(self, X, y):
-        scores = []
+        Y = Parser.tokenize_tweets(X)
+        X = Y[0]
         X_tokens = self.tokenizer.texts_to_sequences(X)
         length = max([len(t.split()) for t in X])
         X_train_pad = pad_sequences(X_tokens, maxlen=length, padding='post')
-        for model in self.models:
-            scores.append(model.evaluate(X_train_pad, CNNClassifier.canonize_y(y)))
-        return scores
-
-    def predict(self, X):
-        X_tokens = self.tokenizer.texts_to_sequences(X)
-        length = max([len(t.split()) for t in X])
-        X_train_pad = pad_sequences(X_tokens, maxlen=length, padding='post')
-        return self.models[0].predict(X_train_pad)
+        return self.model.evaluate(X_train_pad, CNNClassifier.canonize_y(y))[1]
 
 
-cnn = CNNClassifier()
-training_set_path = os.path.join(OUT_DIR_PATH, 'training_set.csv')
-X, y = Parser.load_csv_to_array(training_set_path)
-S, V = split_training_validation_sets(X, y, 0.7)
-cnn.fit(*S)
-cnn.score(*V)
-print(cnn.score(*V))
+if __name__ == "__main__":
+    cnn = CNNClassifier()
+    training_set_path = os.path.join(OUT_DIR_PATH, 'training_set.csv')
+    X, y = Parser.load_csv_to_array(training_set_path)
+    S, V = split_training_validation_sets(X, y, 0.7)
+    cnn.fit(*S)
+    print("Evaluation on test")
+    print(cnn.score(*V))
+    print("Evaluation on train")
+    print(cnn.score(*S))
